5c064b32eb2e6f6bbb6c69066b704568dab642a3
Larry <larry.hixenbaugh@gmail.com>
2023-02-01 23:10:41 -0500

    gamepad analog stick support

--- src/input_handler.py
+++ src/input_handler.py
@@ -2,8 +2,9 @@ import pyxel
 
 class InputHandler:
     def __init__(self):
-        
 
+        self.gamepad_deadzone = 8000
+        
         self.key_map = {
             'up':[pyxel.KEY_I,pyxel.KEY_UP,pyxel.GAMEPAD1_BUTTON_DPAD_UP],
             'down':[pyxel.KEY_K,pyxel.KEY_DOWN,pyxel.GAMEPAD1_BUTTON_DPAD_DOWN],
@@ -19,7 +20,24 @@ class InputHandler:
             print('Input Handler Error: undefined key')
             return False
         
-        return any([pyxel.btn(key) for key in self.key_map[key_val]])
+        if any([pyxel.btn(key) for key in self.key_map[key_val]]):
+            return True
+
+        # check analog stick
+        
+        gx = pyxel.btnv(pyxel.GAMEPAD1_AXIS_LEFTX)
+        gy = pyxel.btnv(pyxel.GAMEPAD1_AXIS_LEFTY)
+
+        #print(gx,gy)
+
+        if key_val == 'right':
+            return gx > self.gamepad_deadzone
+        elif key_val == 'left':
+            return gx < -self.gamepad_deadzone
+        elif key_val == 'up':
+            return gy < -self.gamepad_deadzone
+        elif key_val == 'down':
+            return gy > self.gamepad_deadzone
 
     def btnp(self,key_val):
         if key_val not in self.key_map:
